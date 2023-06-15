2182949c4461bb831e812b99fc21d60e8da8d50f
sudharshan2151 <123924081+sudharshan2151@users.noreply.github.com>
2023-06-15 09:35:38 +0530

    admin done

---
+++ TripSage/src/com/masai/main/Main.java
@@ -0,0 +1,5 @@
+package com.masai.main;
+
+public class Main {
+
+}
---
+++ TripSage/src/com/masai/service/Admin.java
@@ -0,0 +1,6 @@
+package com.masai.service;
+
+public interface Admin {
+	static String username = "admin";
+	static String password = "admin123";
+}
